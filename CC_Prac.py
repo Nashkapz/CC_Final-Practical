f63b913b9e076da3d7f13982aaac112f4542f057
Sourav Kapoor <136272329+Nashkapz@users.noreply.github.com>
2024-04-30 10:28:26 +0530

    Create CC_Prac.py

---
+++ CC_Prac.py
@@ -0,0 +1,6 @@
+print('21BCS10417 - Sourav')
+print('Program to add two numbers')
+num1 = int(input('Enter any Number - '))
+num2 = int(input('Enter any Number - '))
+sum = num1 + num2
+print('Sum of Numbers is - ', sum)
